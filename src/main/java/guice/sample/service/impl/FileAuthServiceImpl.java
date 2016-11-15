80baeda46642b78f65c8351b70a08c3d58a156ce
andygulin <andygulin@hotmail.com>
2016-11-15 13:09:35 +0800

    ...

--- src/main/java/guice/sample/service/impl/FileAuthServiceImpl.java
+++ src/main/java/guice/sample/service/impl/FileAuthServiceImpl.java
@@ -1,12 +1,6 @@
 package guice.sample.service.impl;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.util.List;
-
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+import java.util.ResourceBundle;
 
 import com.google.inject.Singleton;
 
@@ -17,16 +11,10 @@ public class FileAuthServiceImpl implements AuthService {
 
 	@Override
 	public boolean login(String username, String password) {
-		Resource resource = new ClassPathResource("auth.txt");
-		List<String> lines = null;
-		try {
-			lines = Files.readAllLines(resource.getFile().toPath(), StandardCharsets.UTF_8);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		String u = lines.get(0);
-		String p = lines.get(1);
-		if (username.equals(u) && password.equals(p)) {
+		ResourceBundle bundle = ResourceBundle.getBundle("auth");
+		String name = bundle.getString("username");
+		String pass = bundle.getString("password");
+		if (username.equals(name) && password.equals(pass)) {
 			return true;
 		}
 		return false;
